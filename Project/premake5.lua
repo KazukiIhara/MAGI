b3ff18d715a057366dbe3718b1e9b55694a6f0b4
KazukiIhara <iharakazuki0829@gmail.com>
2025-01-08 23:54:49 +0900

    externalsを追加

--- Project/premake5.lua
+++ Project/premake5.lua
@@ -1,21 +1,32 @@
--- premake5.lua
-workspace "MAGI"
+workspace "MAGIEngine"
    configurations { "Debug", "Release" }
-   platforms { "x64" } -- プラットフォームの設定
+   platforms { "x64" }
+   startproject "MAGI" -- デフォルトのスタートプロジェクトを指定
 
-local externals_dir = "Externals"  -- 外部ライブラリディレクトリの基準パス
+   -- 出力ディレクトリの設定
+   targetdir "../generated/bin/%{cfg.buildcfg}/%{cfg.platform}"  -- 実行ファイルの出力先
+   objdir "../generated/obj/%{cfg.buildcfg}/%{cfg.platform}"     -- 中間ファイルの出力先
+
+
+-- MAGIの設定
 
 project "MAGI" -- プロジェクト名
    kind "WindowedApp" -- デスクトップアプリケーションに設定
    language "C++"
    cppdialect "C++20" -- 言語の設定
 
-   targetdir "../generated/bin/%{cfg.buildcfg}/%{cfg.platform}"  -- 実行ファイルの出力先
-   objdir "../generated/obj/%{cfg.buildcfg}/%{cfg.platform}"     -- 中間ファイルの出力先
-   
-   includedirs { "Engine", "App","Externals/assimp/include" } -- 追加のインクルードパス
-
-   files { "**.h", "**.cpp" }
+   files { 
+      "*.cpp",
+      "*.h",
+      "Engine/**.cpp",
+      "Engine/**.h",
+   }
+      
+   includedirs { 
+   "Engine",
+   "App",
+   "Externals/assimp/include",
+    } -- 追加のインクルードパス
 
    warnings "High" -- 警告レベル3を設定
 
@@ -28,8 +39,16 @@ project "MAGI" -- プロジェクト名
       symbols "On"
       fatalwarnings { "All" }  -- 警告をエラーとして扱う
       staticruntime "On"  -- 静的ランタイム（/MTd）
+      linkoptions { "/IGNORE:4049", "/IGNORE:4099" } -- 指定したリンカーの警告を無視
+      libdirs { "Externals/assimp/lib/Debug" } -- デバッグ用追加のライブラリディレクトリ
+      links { "assimp-vc143-mtd" } -- デバッグ用ライブラリ
 
    filter "configurations:Release"
       defines { "NDEBUG" }
       optimize "On"
       staticruntime "On"  -- 静的ランタイム（/MT）
+      linkoptions { "/IGNORE:4049", "/IGNORE:4099" } -- 指定したリンカーの警告を無視
+      libdirs { "Externals/assimp/lib/Release" } -- リリース用追加のライブラリディレクトリ
+      links { "assimp-vc143-mt" } -- リリース用ライブラリ
+
+
