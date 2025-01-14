45f216f4d7559d48526bb970586b082bb894e189
KazukiIhara <iharakazuki0829@gmail.com>
2025-01-14 23:14:36 +0900

    空のSwapChainクラス作成

---
+++ Project/Engine/DirectX/SwapChain/SwapChain.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// C++
+#include <cmath>
+#include <cassert>
+#include <memory>
+
+// DirectX
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+// 自作ファイル
+#include "ComPtr/ComPtr.h"
+
+// 前方宣言
+class WindowApp;
+class DXGI;
+class DirectXCommand;
+class RTVManager;
