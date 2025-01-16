294d46532d925974c72ed8362756339a7489ad49
KazukiIhara <iharakazuki0829@gmail.com>
2025-01-17 01:17:03 +0900

    コメントを少し変更

--- Project/Engine/IScene/IScene.h
+++ Project/Engine/IScene/IScene.h
@@ -19,13 +19,12 @@ public:
 
 	virtual ~IScene() = default;
 
-	// 毎フレームの更新
-	virtual void Update() = 0;
-	// 毎フレームの描画
-	virtual void Draw() = 0;
-
 	// 初期化
 	virtual void Initialize() = 0;
+	// 更新
+	virtual void Update() = 0;
+	// 描画
+	virtual void Draw() = 0;
 	// 終了
 	virtual void Finalize() = 0;
 
