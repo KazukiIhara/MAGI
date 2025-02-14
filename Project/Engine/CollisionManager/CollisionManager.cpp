205fc536f91c337cade39f1b44e9a1cb16d29a22
KazukiIhara <iharakazuki0829@gmail.com>
2025-02-15 04:09:15 +0900

    コリジョンマネージャの関数を追加

--- Project/Engine/CollisionManager/CollisionManager.cpp
+++ Project/Engine/CollisionManager/CollisionManager.cpp
@@ -3,8 +3,21 @@
 #include <cassert>
 
 CollisionManager::CollisionManager() {
+
 }
 
 void CollisionManager::Update() {
+
+}
+
+void CollisionManager::AddCollider(BaseCollider3D* baseCollider3D) {
+
+}
+
+void CollisionManager::CheckCollisionPair(BaseCollider3D* colliderA, BaseCollider3D* colliderB) {
+
+}
+
+void CollisionManager::CheckAllCollisions() {
 
 }
--- Project/Engine/CollisionManager/CollisionManager.h
+++ Project/Engine/CollisionManager/CollisionManager.h
@@ -1,12 +1,23 @@
 #pragma once
 
+#include "3D/Colliders3D/BaseCollider3D/BaseCollider3D.h"
 
+/// <summary>
+/// コリジョンマネージャ
+/// </summary>
 class CollisionManager {
 public:
 	CollisionManager();
 	~CollisionManager() = default;
 
+	// 更新
 	void Update();
+	// チェックするコライダーを追加
+	void AddCollider(BaseCollider3D* baseCollider3D);
+	// ペア同士の当たり判定をチェック
+	void CheckCollisionPair(BaseCollider3D* colliderA, BaseCollider3D* colliderB);
+	// すべてのコライダー同士の当たり判定チェック
+	void CheckAllCollisions();
 
 private:
 };
