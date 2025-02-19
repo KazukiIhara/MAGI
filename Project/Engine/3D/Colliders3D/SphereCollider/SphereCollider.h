53115edfd0da112ea372cc2f7c33f93c0a2aedbe
KazukiIhara <iharakazuki0829@gmail.com>
2025-02-19 23:40:37 +0900

    コライダーの生成はできるようになった 次はゲームオブジェクトとサウンドの順番に実装

--- Project/Engine/3D/Colliders3D/SphereCollider/SphereCollider.h
+++ Project/Engine/3D/Colliders3D/SphereCollider/SphereCollider.h
@@ -2,6 +2,9 @@
 
 #include "3D/Colliders3D/BaseCollider3D/BaseCollider3D.h"
 
+/// <summary>
+/// 球体コライダー
+/// </summary>
 class SphereCollider:public BaseCollider3D {
 public:
 	SphereCollider(const std::string& name,uint32_t id, Collider3DType type);
