23afd7458e5eafdd789c864735e2cb067179de0c
KazukiIhara <iharakazuki0829@gmail.com>
2025-02-21 20:42:36 +0900

    ゲームオブジェクトマネージャを追加

---
+++ Project/Engine/GameObject3DManager/GameObject3DManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class GameObjectManager {
+public:
+	GameObjectManager();
+	~GameObjectManager();
+
+private:
+
+};
+
+GameObjectManager::GameObjectManager() {}
+
+GameObjectManager::~GameObjectManager() {}
