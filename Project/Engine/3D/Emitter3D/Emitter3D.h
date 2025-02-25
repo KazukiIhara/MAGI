0989495f3f496b33df525ef0590a7c2902ade88c
KazukiIhara <iharakazuki0829@gmail.com>
2025-02-25 21:16:23 +0900

    エミッターのクラスを作成

--- Project/Engine/3D/Emitter3D/Emitter3D.h
+++ Project/Engine/3D/Emitter3D/Emitter3D.h
@@ -1,12 +1,23 @@
 #pragma once
 
 #include "3D/Base3D/WorldEntity/WorldEntity.h"
+#include "3D/ParticleGroups3D/BaseParticleGroup3D/BaseParticleGroup3D.h"
 
+/// <summary>
+/// 3Dパーティクルエミッター
+/// </summary>
 class Emitter3D :public WorldEntity {
 public:
 	Emitter3D();
 	~Emitter3D();
 
+	// 初期化
+	void Initialize();
+
+	// 更新
+	void Update();
+
 private:
+	// パーティクルリスト
 
 };
