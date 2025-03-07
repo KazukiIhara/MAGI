cbba45f56bfb8083dd51b0432e10bb174d715d40
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-03-07 18:00:14 +0900

    レンダーテクスチャ―マネージャを作るよ

--- Project/Engine/RenderTextureManager/RenderTextureManager.h
+++ Project/Engine/RenderTextureManager/RenderTextureManager.h
@@ -1,25 +1,10 @@
 #pragma once
 
 // C++
-#include <cmath>
-#include <cassert>
 #include <memory>
 
-// DirectX
-#include <d3d12.h>
-#include <dxgi1_6.h>
-
 // MyHedder
-#include "DirectX/ComPtr/ComPtr.h"
-#include "Enums/BlendModeEnum.h"
-#include "Enums/PostEffectPipelineEnum.h"
-#include "Structs/ColorStruct.h"
-
-// 前方宣言
-class DXGI;
-class DirectXCommand;
-class RTVManager;
-class SRVUAVManager;
+#include "RenderTextures/BaseRenderTexture/BaseRenderTexture.h"
 
 /// <summary>
 /// レンダーテクスチャマネージャ
