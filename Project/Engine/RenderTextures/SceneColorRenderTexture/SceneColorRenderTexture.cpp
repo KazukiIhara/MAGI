719765bfae4bd9f8b2d0c448a4ab11f5fe742d71
KazukiIhara <iharakazuki0829@gmail.com>
2025-04-17 02:18:37 +0900

    謎のFPS低下減少

--- Project/Engine/RenderTextures/SceneColorRenderTexture/SceneColorRenderTexture.cpp
+++ Project/Engine/RenderTextures/SceneColorRenderTexture/SceneColorRenderTexture.cpp
@@ -8,9 +8,9 @@ SceneColorRenderTexture::~SceneColorRenderTexture() {
 
 void SceneColorRenderTexture::Initialize() {
 	// シーンカラー描画用の標準フォーマットで初期化
-	BaseRenderTexture::Initialize(
+	BaseRenderTexture::Create(
 		DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
 		D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET,
-		Vector4(0.0f, 0.0f, 0.0f, 1.0f)
+		Vector4(0.3f, 0.3f, 0.3f, 1.0f)
 	);
 }
