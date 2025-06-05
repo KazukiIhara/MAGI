8a44f7ac3ce6e35b8958bf3bd28d9b13839129fa
KazukiIhara <iharakazuki0829@gmail.com>
2025-06-06 01:59:24 +0900

    ゲームオブジェクトクラスを追加

---
+++ Project/Engine/3D/GameObject3D/GameObject3D.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/// <summary>
+/// シーン上のオブジェクト
+/// </summary>
+class GameObject3D {
+public:
+	GameObject3D();
+	~GameObject3D();
+
+private:
+
+};
+
+GameObject3D::GameObject3D() {}
+
+GameObject3D::~GameObject3D() {}
